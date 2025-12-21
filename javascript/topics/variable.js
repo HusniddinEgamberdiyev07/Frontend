e0a7d48cd524341e84569668efd530884fab41e4
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2025-12-21 19:23:50 +0500

    Changed variable topic and added explanations

--- javascript/topics/variable.js
+++ javascript/topics/variable.js
@@ -1,1 +1,37 @@
-console.log("Hello World")
+// A variable is named storage for data
+
+// To create a variable use let keyword then name
+
+// let name;
+let fruit;
+
+// Put value using assignment operator "="
+
+// name = value
+fruit = "apple";
+console.log(fruit);     // We can access value using variable name
+
+// We can combine variable declaration and into one single line.
+
+let user = "Husniddin";
+
+
+
+// Constants
+// To declare a constant (you cannot change it) variable use const keyword instead of let.
+
+const PI = 3.14     // You can't change the value.
+console.log(PI);
+
+
+
+// var 
+// It is an old-school declaration. You don't use it.
+
+
+
+// Variable naminrg:
+
+// Variable name must contain only letters, numbers or symbols $ and _
+// The first character must not be number.
+// You can't use names which are used by language itself like let.
