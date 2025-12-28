71ecc4cdfcf45edcbb565571a6226fbef3e793c4
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2025-12-28 14:39:54 +0500

    Starting new topic recursion

---
+++ javascript/topics/recursion&stack.js
@@ -0,0 +1,41 @@
+function pow(x, n){
+    let result = 1;
+    for(let i=0; i<x; i++){
+        result *= n;
+    }
+    return result;
+}
+
+console.log(pow(3, 3));
+
+function pow2(x, n){
+    if(x === 1){
+        return n;
+    }else{
+        return n * pow(x - 1, n)
+    }
+}
+console.log(pow2(3,3));
+
+// pow2(3, 3) => 3 * pow2(2, 3)
+// pow2(2, 3) => 3 * pow2(1, 3)
+// pow2(1, 3) => 3
+
+// 3 * 3 * 3
+
+// Recursion reduces a function call to simpler one, then even more simpler, 
+// and until the result becomes obvious.
+
+// Recursion is usually shorter.
+
+function pow3(x, n){
+    return x === 1 ? n : n * pow3(x-1, n) 
+}
+
+console.log(pow3(4, 2));
+
+// Maximal number of nested calls (including first one) is called recursion depth.
+// In pow function it will be x.
+// Recursion depth is limited by Js engine. It should be 10000.
+
+
