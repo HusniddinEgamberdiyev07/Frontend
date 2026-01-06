83fc7ff9e6753a00dde7b5718f690646bdd616ab
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-01-06 12:36:32 +0500

    reviewed object basics

--- javascript/practise.js
+++ javascript/practise.js
@@ -166,3 +166,190 @@ console.log(addLines(1, 2));
 
 let addOne = num => num+1;
 console.log(addOne(2));
+
+
+// -- Objects review --
+
+// empty object:
+let obj1 = {};
+let obj2 = new Object();
+
+// objects with key & value.
+
+let user = {
+    name:"Husniddin",
+    age:18,
+    "can fly":false,
+};
+
+let key_word = {
+    let:"This is variable keyword",
+};
+
+// read, add, remove properties:
+
+// use dot and key to read.
+
+console.log(user.name);
+
+// obj.key = value; to add new qproperties:
+
+user.dead = false;
+user.married = false;
+
+console.log(user);
+
+// delete obj.key to delete property
+
+delete user.married;
+delete user.dead;
+
+console.log(user);
+
+// multiword no dot.
+// use obj["key"];
+
+// read obj["key"]
+// add obj["key"] = value;
+// remove delete obj["key"];
+
+user["hates the world"] = "true"
+console.log(user["can fly"]);
+console.log(user);
+
+delete user["can fly"];
+
+console.log(user);
+
+let user2 = {
+    "name":"Husniddin",
+};
+
+console.log(user2.name);
+console.log(user2["name"]);
+
+user2["name"] = "Die";
+
+console.log(user2.name);
+
+// benifits of [];
+// You can use them to name a key.
+
+let key_name = "key";
+let key_name2 = "key2";
+
+let dynamic_keys = {
+    [key_name]:"value"
+};
+
+console.log(dynamic_keys);
+
+dynamic_keys[key_name2] = "value2";
+
+console.log(dynamic_keys);
+
+// shorthand
+
+let name = "Husniddin";
+let age2 = 18;
+
+let user3 = {
+    name,
+    age2
+}
+
+console.log(user3);
+
+// in 
+
+console.log("hell" in user3);       // false
+
+let obj_loop = {
+    name:"Husniddin",
+    age:18
+}
+
+let obj_loop2 = {};
+
+for(let key in obj_loop){
+    obj_loop2[key] = obj_loop[key];
+}
+
+console.log(obj_loop, obj_loop2, obj_loop === obj_loop2);
+
+let obj_first = {};
+let obj_second = obj_first;
+let obj_third = {};
+
+console.log(obj_first === obj_second);      // true
+console.log(obj_first === obj_third);       // false
+
+let fruit1 = {
+    name:"apple",
+    color:"red",
+}
+
+let fruit2 = {
+    name:"water-melon",
+    color:"green",
+    shape:"round",
+}
+
+let fruit3 = {
+    "is fresh":true
+};
+
+Object.assign(fruit3, fruit2, fruit1)
+
+console.log(fruit3);
+
+let clone_obj = {
+    message:"Hello",
+};
+
+let clone_obj2 = Object.assign({}, clone_obj);
+
+console.log(clone_obj2, clone_obj, clone_obj === clone_obj2);
+
+let nested_obj = {
+    "child obj":{
+        message:"I am child",
+    }
+}
+
+let nested_obj2 = structuredClone(nested_obj);
+
+console.log(nested_obj, nested_obj2, nested_obj["child obj"] === nested_obj2["child obj"]);
+
+
+
+// create empty obj:
+// {};
+// new Object();
+
+// read -> obj.key
+// add or change -> obj.key = value;
+// delete obj.key;
+
+// read -> obj["key"]
+// add or change -> obj["key"] = value;
+// remove -> delete obj["key"];
+
+// we can use [] to give dynamic keys.
+
+// just writing key and not giving value means key:key.
+
+// in checks property exist or not. "checking key" in obj;
+
+// variable  stores only address of an object in memory. 
+// let obj2 = obj; it takes only the address of the obj and 
+// if we change either of them it will change the object in memory so 
+// change will be on both of them.
+
+// objects are equal if they refrence to the same address.
+
+// we can change const objects because we are not changing the value which variable has.
+
+// we can clone objects using loops.
+// we can clone objects using assign method. Object.assign(obj1, obj2, ...)
+// we can nested clone using structuredClone(obj);
